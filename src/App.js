4cbde264d0085a16209b715a16435141ae602e05
abhijan2402 <abhishek.jangid643@gmail.com>
2023-06-11 14:32:28 +0530

    First Push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import image from './image.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>hi</h1> */}
+      <div className='MainApp'>
+        <div className='InnerMainDiv1'>
+          <div className='Innerh1'>
+            <h1>ShppName</h1>
+          </div>
+          <div className='InnerP'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?</p>
+          </div>
+          <div className='Innerbtn'>
+            <button className='ShopBtn'>Visit Shop</button>
+          </div>
+        </div>
+        <div className='InnerMainDiv2'>
+          <img src={image} className='img_to_view' />
+        </div>
+      </div>
     </div>
   );
 }
